158bec0fbf18b6320974a2c60fdbf84d6c6823eb
Rahul Ram Kumar Balajee <rahrkb4@gmail.com>
2022-03-23 22:48:44 +0530

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/rbalajee/NOC/pkg/config"
+	"github.com/rbalajee/NOC/pkg/handlers"
+	"github.com/rbalajee/NOC/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+
+var session *scs.SessionManager
+
+func main() {
+
+	//Change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println("Starting application on", portNumber)
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
